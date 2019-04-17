8c9b335c236a8e0baff84c095f8dac87d620495e
Carl <carl.paton@gmail.com>
2019-04-17 12:55:52 +1200

    first commit

---
+++ EventLogger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using EventLoggerHoe;
+
+namespace EventLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create log
+            Console.WriteLine("What log would you like to create? (You need to run as `Administrator`)");
+            var log = Console.ReadLine();
+
+            if (!EventLog.SourceExists(log))
+            {
+                EventLog.CreateEventSource(log, log);
+                Console.WriteLine("Creating log: {0}", log);
+            }
+            else
+                Console.WriteLine("Log already exists: {0}", log);
+
+            // Write to log ~ this is in `EventLoggerHoe.dll` so that it can easily be added to any solution
+            var message = "Some debug message";
+            new WriteEntry(log, 99).Log(message);
+
+            Console.WriteLine("END: Sweet I wrote `{0}` to log {1}", message, log);
+            Console.Read();
+        }
+    }
+}
---
+++ EventLoggerHoe/WriteEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EventLoggerHoe
+{
+    public class WriteEntry
+    {
+        private readonly string _logName;
+        private readonly int _eventId;
+
+        public WriteEntry(string logName, int eventId)
+        {
+            _logName = logName;
+            _eventId = eventId;
+        }
+
+        public void Log(Exception ex, EventLogEntryType type = EventLogEntryType.Error)
+        {
+            var message = string.Format("{0}\n\n{1}",
+                ex.Message,
+                ex.StackTrace);
+
+            EventLog.WriteEntry(_logName, message, type, _eventId);
+        }
+
+        public void Log(string message)
+        {
+            EventLog.WriteEntry(_logName, message, EventLogEntryType.Information, _eventId);
+        }
+    }
+}
